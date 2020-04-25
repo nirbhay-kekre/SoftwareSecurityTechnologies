d9e17f79dab77fa8f38aae3bea075004ac75f104
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2020-04-24 19:17:13 -0700

    separating server into processes, and droping the privilege of child which processes the request from client

--- assignment1/server.c
+++ assignment1/server.c
@@ -1,59 +1,105 @@
 // Server side C/C++ program to demonstrate Socket programming 
 #include <unistd.h> 
 #include <stdio.h> 
-#include <sys/socket.h> 
+#include <sys/socket.h>
+#include <sys/types.h> 
+#include <sys/wait.h>
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
+#include <pwd.h>
 #define PORT 80 
 int main(int argc, char const *argv[]) 
 { 
+    int pipe_fd[2];
+    pid_t fork_pid;
     int server_fd, new_socket, valread; 
     struct sockaddr_in address; 
     int opt = 1; 
     int addrlen = sizeof(address); 
     char buffer[1024] = {0}; 
     char *hello = "Hello from server"; 
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-       
-    // Forcefully attaching socket to the port 80 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
-    { 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    } 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
-       
-    // Forcefully attaching socket to the port 80 
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    } 
-    valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
+    const char *nobody = "nobody";
+    // creating unidirectional pipe between parent and child 
+    pipe(pipe_fd);
+    switch(fork_pid = fork()){
+    case -1:
+        //fork failed
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    case 0:
+        // child process : processes data provided by parent
+        close(pipe_fd[1]); //closing write end of the pipe in child
+        struct passwd *pwd;
+        // retrieving user information for nobody user
+        if( (pwd = getpwnam(nobody)) == NULL){
+            perror("getpawnam failed for nobody user"); 
+            exit(EXIT_FAILURE); 
+        }
+        //dropping group privileges
+        if(setgid(pwd->pw_gid) != 0){
+            //setgid failed
+            perror("set group id failed on dropping group privilege"); 
+            exit(EXIT_FAILURE);
+        }
+        //dropping user privileges
+        if(setuid(pwd->pw_uid) != 0){
+            //setuid failed
+            perror("set uid failed on dropping user privilege"); 
+            exit(EXIT_FAILURE); 
+        }
+        //reading data from parent
+        valread = read(pipe_fd[0], buffer, 1024);
+        //processing data from parent
+        printf("%s\n",buffer ); 
+        break;
+    default:
+        // parent
+        close(pipe_fd[0]); //closing read end of the pipe in parent
+        // Creating socket file descriptor 
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+        { 
+            perror("socket failed"); 
+            exit(EXIT_FAILURE); 
+        } 
+        
+        // Forcefully attaching socket to the port 80 
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                    &opt, sizeof(opt))) 
+        { 
+            perror("setsockopt"); 
+            exit(EXIT_FAILURE); 
+        } 
+        address.sin_family = AF_INET; 
+        address.sin_addr.s_addr = INADDR_ANY; 
+        address.sin_port = htons( PORT ); 
+        
+        // Forcefully attaching socket to the port 80 
+        if (bind(server_fd, (struct sockaddr *)&address,  
+                                    sizeof(address))<0) 
+        { 
+            perror("bind failed"); 
+            exit(EXIT_FAILURE); 
+        } 
+        if (listen(server_fd, 3) < 0) 
+        { 
+            perror("listen"); 
+            exit(EXIT_FAILURE); 
+        } 
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                        (socklen_t*)&addrlen))<0) 
+        { 
+            perror("accept"); 
+            exit(EXIT_FAILURE); 
+        } 
+        valread = read( new_socket , buffer, 1024); 
+        //providing data to child via pipe 
+        write(pipe_fd[1], buffer , 1024);
+        int child_exit_status;
+        //waiting for child to finish processing
+        waitpid(fork_pid, &child_exit_status, 0);
+        send(new_socket , hello , strlen(hello) , 0 ); 
+        printf("Hello message sent\n"); 
+    }
     return 0; 
 } 
