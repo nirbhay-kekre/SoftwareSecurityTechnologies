311bde3ee1d6981d6f9bfc7c8dc9696b73dcad2e
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2020-04-24 20:10:00 -0700

    dropping privileges of the process which talks over the network. Also doing chroot to empty directory for unprivileged process

--- assignment1/server.c
+++ assignment1/server.c
@@ -28,41 +28,17 @@ int main(int argc, char const *argv[])
         perror("fork failed");
         exit(EXIT_FAILURE);
     case 0:
-        // child process : processes data provided by parent
-        close(pipe_fd[1]); //closing write end of the pipe in child
-        struct passwd *pwd;
-        // retrieving user information for nobody user
-        if( (pwd = getpwnam(nobody)) == NULL){
-            perror("getpawnam failed for nobody user"); 
-            exit(EXIT_FAILURE); 
-        }
-        //dropping group privileges
-        if(setgid(pwd->pw_gid) != 0){
-            //setgid failed
-            perror("set group id failed on dropping group privilege"); 
-            exit(EXIT_FAILURE);
-        }
-        //dropping user privileges
-        if(setuid(pwd->pw_uid) != 0){
-            //setuid failed
-            perror("set uid failed on dropping user privilege"); 
-            exit(EXIT_FAILURE); 
-        }
-        //reading data from parent
-        valread = read(pipe_fd[0], buffer, 1024);
-        //processing data from parent
-        printf("%s\n",buffer ); 
-        break;
-    default:
-        // parent
-        close(pipe_fd[0]); //closing read end of the pipe in parent
+        // child process: this process talks over the network in reduced privilege
+        //changing root to /var/empty
+        chroot("/var/empty");
+        close(pipe_fd[0]); //closing read end of the pipe in child process
         // Creating socket file descriptor 
         if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
         { 
             perror("socket failed"); 
             exit(EXIT_FAILURE); 
         } 
-        
+
         // Forcefully attaching socket to the port 80 
         if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
                                                     &opt, sizeof(opt))) 
@@ -81,6 +57,26 @@ int main(int argc, char const *argv[])
             perror("bind failed"); 
             exit(EXIT_FAILURE); 
         } 
+        //dropping privileges after bind since we require root privileges for binding to port 80
+        struct passwd *pwd;
+        // retrieving user information for nobody user
+        if( (pwd = getpwnam(nobody)) == NULL){
+            perror("getpawnam failed for nobody user"); 
+            exit(EXIT_FAILURE); 
+        }
+        //dropping group privileges
+        if(setgid(pwd->pw_gid) != 0){
+            //setgid failed
+            perror("set group id failed on dropping group privilege"); 
+            exit(EXIT_FAILURE);
+        }
+        //dropping user privileges
+        if(setuid(pwd->pw_uid) != 0){
+            //setuid failed
+            perror("set uid failed on dropping user privilege"); 
+            exit(EXIT_FAILURE); 
+        }
+        
         if (listen(server_fd, 3) < 0) 
         { 
             perror("listen"); 
@@ -95,11 +91,19 @@ int main(int argc, char const *argv[])
         valread = read( new_socket , buffer, 1024); 
         //providing data to child via pipe 
         write(pipe_fd[1], buffer , 1024);
+        send(new_socket , hello , strlen(hello) , 0 ); 
+        printf("Hello message sent\n"); 
+        break;
+    default:
+        // parent process : processes data provided by parent
+        close(pipe_fd[1]); //closing write end of the pipe in parent
+        //reading data from parent
+        valread = read(pipe_fd[0], buffer, 1024);
+        //processing data from parent
+        printf("%s\n",buffer ); 
         int child_exit_status;
         //waiting for child to finish processing
         waitpid(fork_pid, &child_exit_status, 0);
-        send(new_socket , hello , strlen(hello) , 0 ); 
-        printf("Hello message sent\n"); 
     }
     return 0; 
 } 
